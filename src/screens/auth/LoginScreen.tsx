886b60bb02efccf6613cefe71a720623a72343d5
sohaibozumaki3-code <sohaibozumaki3@gmail.com>
2026-03-26 23:23:00 +0100

    Update LoginScreen to use route params for role

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 
-export const LoginScreen = ({ navigation }) => {
+export const LoginScreen = ({ route, navigation }) => {
+
+  const role = route.params?.role;
 
   return (
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-      
+
       <Text style={{fontSize:24}}>Login 🔐</Text>
+      <Text>Role: {role}</Text>
 
-      {/* Customer */}
       <Button 
-        title="Login as Customer"
-        onPress={() => navigation.replace("Main", { role: "customer" })}
-      />
-
-      {/* Admin */}
-      <Button 
-        title="Login as Admin"
-        onPress={() => navigation.replace("Main", { role: "admin" })}
+        title="Enter App"
+        onPress={() => navigation.replace("Main", { role })}
       />
 
     </View>
